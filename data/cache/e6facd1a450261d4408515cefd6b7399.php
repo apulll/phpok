44f4fcf2f66919de6f953ea1796ad190e3e00389
Capul <1216065136@qq.com>
2017-04-06 09:57:23 +0800

    nothing

--- data/cache/e6facd1a450261d4408515cefd6b7399.php
+++
@@ -1,1 +0,0 @@
-<?php exit();?>a:15:{s:2:"id";s:1:"1";s:7:"site_id";s:1:"1";s:9:"parent_id";s:1:"0";s:6:"status";s:1:"1";s:5:"title";s:12:"资讯分类";s:5:"taxis";s:2:"10";s:8:"tpl_list";s:0:"";s:11:"tpl_content";s:0:"";s:5:"psize";s:1:"0";s:9:"seo_title";s:0:"";s:12:"seo_keywords";s:0:"";s:8:"seo_desc";s:0:"";s:10:"identifier";s:8:"infocate";s:3:"tag";s:0:"";s:3:"url";s:21:"article/infocate.html";}
